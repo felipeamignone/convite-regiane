b135abe3d700d7bb88503e0e41e7266b55957852
felipe <dev.felipemignone@gmail.com>
2025-01-11 17:08:33 -0300

    recive guests names by query params

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,18 +6,58 @@ $(document).ready(function () {
     $("#response").hide();
   });
 
-  $("#rsvp-form").on("submit", function (e) {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  let namesString = params.names;
+  let namesList = [];
+  if (namesString) {
+    namesList = namesString.split(",");
+
+    let formCheckboxes = "";
+    namesList.forEach((name) => {
+      $("#guests-text").append(`<p class="mb-1">${name}</p>`);
+      formCheckboxes += `
+       <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="${name}" name="guests" value="${name}">
+            <label class="form-check-label" for="${name}">${name}</label>
+        </div>
+      `;
+    });
+    $("#guests-text").append(
+      `<p class="mb-1 mt-3">
+        Venha${namesList.length > 1 && 'm'} comemorar comigo <br />
+        este dia especial
+      </p>`
+    );
+
+    $("#guest-list").append(formCheckboxes);
+  }
+
+  $("#guest-form").on("submit", function (e) {
     e.preventDefault();
-    const name = $("#name").val();
+    let confirmedGuests = [];
+
+    $('input[name="guests"]:checked').each(function () {
+      const guestName = $(this).val();
+      confirmedGuests.push(guestName);
+    });
 
     const submitButton = $("#submit-button");
 
     submitButton.prop("disabled", true);
 
+    if (confirmedGuests.length === 0) {
+      alert("Selecione pelo menos um convidado.");
+      submitButton.prop("disabled", false);
+      return;
+    }
+
     $.ajax({
-      url: "https://script.google.com/macros/s/AKfycbyQGXu1T0BnnNRKC28YOYpVIKUFR-X2yultCVwzzfOpjOeNqif7xsphX-AySfzEjE82/exec",
+      url: "https://script.google.com/macros/s/AKfycbwqCMBaIX2dUmK4xSUXwd2DQmnhFp-hW97LAke5Ds-_5yL84hpVeVaqpZodEv93KnZq/exec",
       method: "POST",
-      data: JSON.stringify({ name: name }),
+      data: JSON.stringify({ names: confirmedGuests }),
       headers: {
         "Content-Type": "text/plain;charset=utf-8",
       },
